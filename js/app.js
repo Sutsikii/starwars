1cefe4d8a7c900df1fc0729b65bcaf6c9303fffb
Sutsikii <nicolas-dutertre@bbox.fr>
2022-09-19 15:43:05 +0200

    Show-Note

--- js/app.js
+++ js/app.js
@@ -16,8 +16,37 @@ addBox.addEventListener("click", () =>
 
 closeIcon.addEventListener("click", () =>
 {
+    titleTag.value = "";
+    descTag.value = "";
     popupBox.classList.remove("show");
 });
+
+function showNotes()
+{
+    document.querySelectorAll(".note").forEach(note => note.remove());
+    notes.forEach((note) =>
+    {
+        let liTag = `        
+        <li class="note">
+            <div class="details">
+                <p>${note.title}</p>
+                <span>${note.description}</span>
+            </div>
+            <div class="bottom-content">
+                <span>${note.date}</span>
+                <div class="settings">
+                    <i class="uil uil-ellipsis-h"></i>
+                    <ul class="menu">
+                        <li><i class="uil uil-pen"></i>Editer</li>
+                        <li><i class="uil uil-trash"></i>Supprimer</li>
+                    </ul>
+                </div>
+            </div>
+        </li>`;
+        addBox.insertAdjacentHTML("afterend", liTag);
+    });
+}
+showNotes();
 
 addBtn.addEventListener("click", e =>
 {
@@ -41,6 +70,8 @@ addBtn.addEventListener("click", e =>
 
         notes.push(noteInfo); //Ajoute la nouvelle note dans notes
         localStorage.setItem("notes", JSON.stringify(notes));
+
+        showNotes();
 
     }
 
